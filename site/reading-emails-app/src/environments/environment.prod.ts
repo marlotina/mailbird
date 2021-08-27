dd74e39e2ce383ff20cfc40ce37310ff6085beaf
marlotina <inaki.arrieta@gmail.com>
2021-08-27 13:20:15 +0200

    update docker deployment

--- site/reading-emails-app/src/environments/environment.prod.ts
+++ site/reading-emails-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  emailApi:"https://localhost:49179",
+  emailApi:"http://localhost:49180",
   itemsPerLoad: 20,
   serverTypes: ['POP3', 'IMAP'],
   encryptions: [{id:'UNENCRYPTED', text:'Unencrypted'},{ id:'SSL_TLS', text:'SSL/TLS'}, { id:'STARTTLS', text:'STARTTLS'}]
--- site/reading-emails-app/src/environments/environment.ts
+++ site/reading-emails-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  emailApi:"https://localhost:49180",
+  emailApi:"http://localhost:44379",
   itemsPerLoad: 20,
   serverTypes: ['POP3', 'IMAP'],
   encryptions: [{id:'UNENCRYPTED', text:'Unencrypted'},{ id:'SSL_TLS', text:'SSL/TLS'}, { id:'STARTTLS', text:'STARTTLS'}]
